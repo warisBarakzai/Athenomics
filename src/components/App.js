5fd3dea1a94940f6a661cf8211565a9f4058c696
Dr <dr@DrdeMacBook-Pro.local>
2020-03-24 17:11:52 -0400

    bootstrap added

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import logo from '../logo.png';
 import './App.css';
 import ipfs from './ipfs';
 import Web3 from 'web3';
+import background from './background.jpg';
 
 class App extends Component {
 
@@ -63,13 +64,21 @@ class App extends Component {
 
 
 
+
+
+  
+
+
+
+
+
   render() {
     return (
-      <div>
+      <div className="container">
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            // href="http://www.dappuniversity.com/bootcamp"
             target="_blank"
             rel="noopener noreferrer"
           >
