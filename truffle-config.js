838a783910ac9aea32b9f73ee92046dbbd14f808
Jeong Soo Yi <jy1560@nyu.edu>
2020-03-28 15:33:49 -0400

    Infura config added via Ropsten Network

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   contracts_directory: "./src/contracts",
   contracts_build_directory: "./src/abi",
   networks: {
+    ropsten: {
+      provider: function(){
+        return new HDWalletProvider(
+          process.env.MNEMONIC, 
+          'https://ropsten.infura.io/${process.env.INFURA_API_KEY}'
+        )
+      },
+      gasPrice: 2500000000,
+      network_id: 3
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
@@ -11,6 +24,12 @@ module.exports = {
     },
     develop: {
       port: 8545
+    }
+  },
+  solc:{
+    optimizer:{
+      enabled: true,
+      runs: 200
     }
   }
 };
