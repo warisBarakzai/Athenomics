b3086740fff08a9d1e3767630791b1a0d23286eb
Dr <garywu2697@gmail.com>
2020-04-08 16:39:52 -0400

    Updates

---
+++ precache-manifest.35c6dc813469d9eb2b53a85896ca93b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4175c1591776233d20b",
+    "url": "/static/css/main.16fc855d.chunk.css"
+  },
+  {
+    "revision": "b4175c1591776233d20b",
+    "url": "/static/js/main.b4175c15.chunk.js"
+  },
+  {
+    "revision": "6ebacac2d575b5a89fbf",
+    "url": "/static/css/1.7f9de69f.chunk.css"
+  },
+  {
+    "revision": "6ebacac2d575b5a89fbf",
+    "url": "/static/js/1.6ebacac2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "a5db97e564b397c8aa609c9ca49d099f",
+    "url": "/index.html"
+  }
+];
